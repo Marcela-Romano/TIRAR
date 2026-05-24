a24c4114e50d4824599301c0cb49dcbc39742040
Marcela-Romano <marcelabeatrizromano@gmail.com>
2026-05-24 03:31:19 -0300

    update tirar_code codigo para git

--- tirar_code.py
+++ tirar_code.py
@@ -1,1 +1,22 @@
-print("tirar")
+print("tirar ahora mismo")
+
+
+    """
+    
+    
+    
+git remote add origin https://github.com/Marcela-Romano/TIRAR.git
+git branch -M main
+git push -u origin main
+
+git remote add origin https://github.com/Marcela-Romano/TIRAR.git
+git branch -M main
+git push -u origin main
+    
+    
+    
+    
+    
+    
+git clone https://github.com/Marcela-Romano/TIRAR.git
+    """
